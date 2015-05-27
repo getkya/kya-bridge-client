e2f82b73b80167395a3562cb2730e81b2fa83b8a
Sam Phippen <samphippen@googlemail.com>
2015-05-27 14:57:42 +0100

    Fix up the client bridge spec

--- spec/kya_bridge_client_spec.rb
+++ spec/kya_bridge_client_spec.rb
@@ -4,8 +4,4 @@ describe KyaBridgeClient do
   it 'has a version number' do
     expect(KyaBridgeClient::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
